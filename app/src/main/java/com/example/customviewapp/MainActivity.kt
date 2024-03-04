80d44b8621a4a22afa3e472f859f0ca877401451
AndreyFadey <Andrjuhafad@yandex.ru>
2024-03-04 17:29:06 +0300

    Revert "Внесены правки после ревью приложения"
    This reverts commit ab4222aed1fc6d9df045782f057897001355d2bf.

--- app/src/main/java/com/example/customviewapp/MainActivity.kt
+++ app/src/main/java/com/example/customviewapp/MainActivity.kt
@@ -6,21 +6,24 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.DecelerateInterpolator
-import android.view.animation.RotateAnimation
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
-
-    private var startDegree = 0F
-    private var finishDegree = 0F
-    private lateinit var rainbowWheel: ImageView
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var handler: Handler
+    private lateinit var imageList: ArrayList<Int>
+    private lateinit var adapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,51 +32,31 @@ class MainActivity : AppCompatActivity() {
         val spinButton = findViewById<Button>(R.id.spinBtn)
         val clearButton = findViewById<Button>(R.id.clearViewBtn)
         val imageView = findViewById<ImageView>(R.id.mainActivityImageView)
-        rainbowWheel = findViewById(R.id.spinnerIV);
+
+        init()
+        setUpTransformer()
 
         spinButton.setOnClickListener {
-            onClickSpin()
+            val randomInt = (0..6).random()
+            val url = getUrl()
+
+            handler.post(runnable(randomInt))
+
+            when (randomInt) {
+                0 -> drawText("Красный", imageView, Color.RED)
+                1 -> loadImage(url, imageView)
+                2 -> drawText("Жёлтый", imageView, Color.YELLOW)
+                3 -> loadImage(url, imageView)
+                4 -> drawText("Голубой", imageView, Color.BLUE)
+                5 -> loadImage(url, imageView)
+                6 -> drawText("Фиолетовый", imageView, R.color.violet)
+                else -> return@setOnClickListener
+            }
+
         }
 
         clearButton.setOnClickListener {
             imageView.setImageDrawable(null)
-        }
-    }
-
-    private fun onClickSpin() {
-        startDegree = finishDegree % 360
-        finishDegree = ((0..3600).random() + 720).toFloat()
-        val rotate = RotateAnimation(
-            startDegree, finishDegree, RotateAnimation.RELATIVE_TO_SELF,
-            0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f
-        )
-        rotate.duration = 3600
-        rotate.fillAfter = true
-        rotate.interpolator = DecelerateInterpolator()
-        rotate.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation) {}
-
-            override fun onAnimationEnd(animation: Animation) {
-                spinResult((360 - finishDegree % 360).toInt())
-            }
-
-            override fun onAnimationRepeat(animation: Animation) {}
-        })
-        rainbowWheel.startAnimation(rotate)
-    }
-
-    private fun spinResult(degree: Int) {
-        val imageUrl = getUrl()
-        val imageView = findViewById<ImageView>(R.id.mainActivityImageView)
-
-        when (degree) {
-            in 310..360 -> drawText("Красный", imageView, Color.RED)
-            in 258..309 -> loadImage(imageUrl, imageView)
-            in 207..257 -> drawText("Жёлтый", imageView, Color.YELLOW)
-            in 155..206 -> loadImage(imageUrl, imageView)
-            in 103..154 -> drawText("Голубой", imageView, Color.BLUE)
-            in 51..102 -> loadImage(imageUrl, imageView)
-            in 0..50 -> drawText("Фиолетовый", imageView, ContextCompat.getColor(this, R.color.violet))
         }
     }
 
@@ -100,5 +83,46 @@ class MainActivity : AppCompatActivity() {
     private fun getUrl(): String {
         val urlRandomizer = (0..999999).random()
         return "https://loremflickr.com/320/240?random=$urlRandomizer"
+    }
+
+    private fun runnable(item: Int) = Runnable {
+        viewPager2.currentItem = item
+        println(viewPager2.currentItem)
+    }
+
+    private fun setUpTransformer() {
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init() {
+        viewPager2 = findViewById(R.id.viewPager2)
+        handler = Handler(Looper.myLooper()!!)
+
+        imageList = arrayListOf(
+            R.drawable.red_square,
+            R.drawable.orange_square,
+            R.drawable.yellow_square,
+            R.drawable.green_square,
+            R.drawable.light_blue_squad,
+            R.drawable.blue_square,
+            R.drawable.purple_square
+        )
+
+        adapter = Adapter(imageList, viewPager2)
+
+        viewPager2.adapter = adapter
+        viewPager2.currentItem = 3
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
     }
 }
